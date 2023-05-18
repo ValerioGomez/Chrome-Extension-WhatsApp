616f5ba863b64601059d92acaadda4f45d74f013
Valerio Gomez <valesitoo@gmail.com>
2023-05-17 19:22:31 -0500

    version 1.0.1
    primera modificacion

--- js.js
+++ js.js
@@ -4,15 +4,24 @@ const input = document.querySelector('#input');
 const abrirURL = () => {
   const numero = input.value;
   if (numero) {
-    window.open(`https://api.whatsapp.com/send/?phone=+51${numero}`, '_blank');
+    window.open(`https://api.whatsapp.com/send/?phone=51${numero}`, '_blank');
   }
-}
+};
 
 button.addEventListener('click', abrirURL);
 
+input.addEventListener('paste', (event) => {
+  event.preventDefault();
+  const clipboardData = event.clipboardData || window.clipboardData;
+  const pastedText = clipboardData.getData('text/plain');
+  const numbersOnly = pastedText.replace(/\D/g, '');
+  input.value = numbersOnly;
+});
+
 input.addEventListener('keypress', (event) => {
-  if (event.keyCode === 13) {
+  if (event.key === 'Enter') {
     abrirURL();
   }
 });
+
 input.focus();
